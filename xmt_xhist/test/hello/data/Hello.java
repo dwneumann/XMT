c39b0ee6e79aaeccf16e1816bb1c6df64175e412
Dean Neumann <dean@rightmesh.io>
2019-01-29 16:39:28 -0800

    whitespace

--- xmt_xhist/test/hello/data/Hello.java
+++ xmt_xhist/test/hello/data/Hello.java
@@ -11,7 +11,7 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License. 
  */
-/* xhist debug FALSE */
+/* xhist debug FALSE */	
 
 import java.lang.Runtime;
 import java.io.DataOutputStream;
@@ -69,4 +69,3 @@ public	class		Hello
     }
 
 }
-
